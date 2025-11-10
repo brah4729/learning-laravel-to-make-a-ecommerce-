b7f09189f60ceff92417a856575d8d64bd912b55
unknown <helloxdlolidc@gmail.com>
2025-11-10 17:44:22 +0700

    fixed soem code so vercel can be used

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            buildDirectory: 'build', 
         }),
         tailwindcss(),
     ],
-});
+
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        emptyOutDir: true,
+    },
+})
